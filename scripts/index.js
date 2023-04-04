b6d59e0da477c319c6403cec69177efc015ab9a4
Dieter Rozek <rozekd@protonmail.com>
2023-04-03 18:37:09 -0700

    Project 3 Stage 3 v1

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,14 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalClose = document.querySelector(".modal__close");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+modalClose.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
